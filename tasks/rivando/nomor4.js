07416a0f865c14ac93f4e42a033436bdbffbf297
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-09 11:53:22 +0700

    feat : jawaban nomor 4

---
+++ tasks/rivando/nomor4.js
@@ -0,0 +1,12 @@
+// a. ada error user not define di app.js baris ke 15 di kolom ke 10, baris 30 dan kolom 10 ,baris 50 dan kolom 2
+//    peyebab hal tersebut adalah karena fuction/sintax tidak menemukan data dari user(Object user) karena ada kesalahan
+//    format maupun karena Object itu sendiri belum di buat
+
+// b. karena array tidak memiliki isi(kosong) menyebabkan menghitung berapa banyak value dalam array tidak bisa di lakukan
+// c. adanya sebuah tutup kurung tidak di gunakan dalam code menyebabkan error
+// d. mirip dengan jawaban b tetapi dengan Object karena tidak adanya sebuath objek yang bernama status di dalam objek
+//    setting menybabkan tersebut
+// e. saya belum tau error ini tapi dari namanya saya ber ansum sikan penyebabnya function berjalan melebihi
+//    kapasitas kode/hardware yang di sediakan dan dari nama fuction bahwasnya ada kemungkinan fuction berjalan berulang
+//    berarti ada kesalahan dalam perulangan
+
