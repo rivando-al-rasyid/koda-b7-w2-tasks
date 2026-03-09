a3af9f436e7edc4cd70142498808c51d2b7e68b2
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-03-09 13:24:36 +0700

    feat: add countArray function

---
+++ tasks/dwiki/tugas3.js
@@ -0,0 +1,17 @@
+export function countArray(arr){
+    if(Array.isArray(arr)){
+        let hasil = 1;
+        for(let i = 0; i < arr.length; i++){
+            if(typeof arr[i] === "number"){
+                hasil *= arr[i]
+            }else{
+                return "Array harus bertipe number"
+            }
+    }
+    return hasil
+    }else{
+        return "Input harus berupa Array"
+    }
+}
+
+console.log(countArray([1,2,3,4,5]))
