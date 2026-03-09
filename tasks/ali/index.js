001ae3cf0d3ea4b619be69d299ea58668d503122
l1mus <alinesezcore@gmail.com>
2026-03-09 12:26:33 +0700

    feat: add function countArray from ./tasks/ali/countArrayFn

--- tasks/ali/index.js
+++ tasks/ali/index.js
@@ -1,10 +1,12 @@
 import { manualBuilInFilter, manualBuilInMap,} from "./manualFnBuildInMethod.js";
 import { cetakAngka } from "./printTable.js"
+import { countArray } from "./countArrayFn.js"
 
 const func = {
   manualBuilInFilter,
   manualBuilInMap,
-  cetakAngka
+  cetakAngka,
+  countArray
 };
 
 export default func;
