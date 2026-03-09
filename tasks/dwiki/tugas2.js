09f74f387b5f2461fb29427afc36761f63c71953
Bernadus Dwiki Adicitra <dwiki.adicitra6@gmail.com>
2026-03-09 13:23:54 +0700

    feat: add cetakTabel function

---
+++ tasks/dwiki/tugas2.js
@@ -0,0 +1,11 @@
+export function cetakTabel(angka){
+    if(typeof angka === "number" && angka > 0){
+        for(let i = 1; i <= 10; i++){
+        console.log(angka +" x "+ i +" = "+angka*i)
+    }
+    }else{
+        console.log("Parameter harus bertipe data nomor dan harus nomor positif") 
+    }
+}
+
+cetakTabel(10)
