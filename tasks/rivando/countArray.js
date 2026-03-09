bb8d78e0e52650e883df10f90d3eabc179e7a386
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-09 14:05:49 +0700

    feat : error Handling

--- tasks/rivando/countArray.js
+++ tasks/rivando/countArray.js
@@ -1,8 +1,17 @@
 export function countArray(arr){
+    if (!Array.isArray(arr)) {
+    throw new TypeError("Argumen pertama harus berupa Array.");
+  }
 
   let hasil = 1;
-  arr.forEach(angka => {
-    hasil = hasil * angka;
-  });
+
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'number' && Number.isNaN(arr[i])) {
+      throw new Error(`Elemen pada indek ${i} bukan angka valid.`);
+    }
+    hasil *= arr[i];
+
+  }
   return hasil;
 }
--- tasks/rivando/nomor1.js
+++ tasks/rivando/nomor1.js
@@ -5,24 +5,66 @@ const produk = [
   { nama: "Monitor", harga: 300, stok: 8 }
 ];
 
+function custArr(arr, callback) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Argumen pertama harus berupa Array.");
+  }
+  if (typeof callback !== "function") {
+    throw new TypeError("Argumen kedua harus berupa Fungsi (callback).");
+  }
 
-
-function cusArr(arr, callback) {
-  const hasil = [];
+  let hasil = [];
   for (let i = 0; i < arr.length; i++) {
-    if (callback(arr[i])){
-      hasil = [...hasil,arr[i] ];
+      try {
+        if (callback(arr[i])) {
+      hasil = [...hasil, arr[i]];
+    }
+
+  } catch (error) {
+      console.error(`Error pada indeks ke-${i}:`, err.message);
+  }
+
+  }
+  return hasil;
+}
+
+function custMap(arr, callback) {
+    if (!Array.isArray(arr)) {
+    throw new TypeError("Argumen pertama harus berupa Array.");
+  }
+    if (typeof callback !== "function") {
+    throw new TypeError("Argumen kedua harus berupa Fungsi (callback).");
+  }
+  let hasil = [];
+  for (let i = 0; i < arr.length; i++) {
+    try {
+    const itemTransformasi = callback(arr[i]);
+    hasil = [...hasil, itemTransformasi];
+
+    } catch (error) {
+      console.error(`Error pada indeks ke-${i}:`, err.message);
     }
   }
   return hasil;
 }
 
-function cusMap(arr, callback) {
-  const hasil = [];
+// Test 1: Cari produk yang stoknya tersedia
+const produkTersedia = custArr(produk, (p) => p.stok > 0);
+console.log("Produk Tersedia:", produkTersedia);
 
-  for (let i = 0; i < arr.length; i++) {
-      const itemTransformasi = callback(arr[i]);
-      hasil = [...hasil, itemTransformasi ];
-    }
-  return hasil;
-}
+// Test 2: Cari produk mahal (harga > 200)
+const produkMahal = custArr(produk, (p) => p.harga > 200);
+console.log("Produk Mahal:", produkMahal);
+
+// Test 3: Ambil daftar nama produk saja
+const daftarNama = custMap(produk, (p) => p.nama);
+console.log("Daftar Nama:", daftarNama);
+
+// Test 4: Hitung nilai total stok (harga * stok)
+const nilaiInventaris = custMap(produk, (p) => {
+  return {
+    nama: p.nama,
+    totalNilai: p.harga * p.stok
+  };
+});
+console.log("Nilai Inventaris:", nilaiInventaris);
