a6cfce559444f41eeb58530a0dc6a0d73af0c8eb
Fakhri Ridho <mightydhiyamatra@yahoo.co.id>
2026-03-09 09:45:05 +0700

    feat: add entrypoint file

---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Task Fakhri
+ */
+
+import fakhri from "./tasks/fakhri/index.js";
+
+console.log("===========================================");
+// Use IIFE here
+(function taskFakhri() {
+  // Convert Celcius to Fahrenheit
+  // Success
+  try {
+    const fahrenheit = fakhri.celciusToFahrenheit(10);
+    console.log(fahrenheit);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const fahrenheit = fakhri.celciusToFahrenheit("10", 30);
+    console.log(fahrenheit);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Counting Changes Nominal
+  // Success
+  try {
+    const changes = fakhri.cashier(250_000, 375_000);
+    console.log(changes);
+  } catch (error) {
+    console.log(error.message);
+  }
+  // Failed
+  try {
+    const changes = fakhri.cashier("250_000", "375_000");
+    console.log(changes);
+  } catch (error) {
+    console.log(error.message);
+  }
+})();
+console.log("===========================================");
