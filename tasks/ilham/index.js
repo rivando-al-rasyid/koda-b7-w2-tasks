2f53b7247a47f9c6340daca9d28b2ca2bc90fae7
ilhammursidi <imursidi37@gmail.com>
2026-03-09 16:36:14 +0700

    feat: add import and function in index.js

---
+++ tasks/ilham/index.js
@@ -0,0 +1,13 @@
+import {countArray} from "./countArray.js";
+import {cetakTable} from "./cetakTabel.js";
+import  {manualMap} from "./manualBuiltinMethod.js";
+import {manualFilter} from "./manualBuiltinMethod.js";
+
+const funct = {
+    manualMap,
+    manualFilter,
+    cetakTable,
+    countArray
+}
+
+export default funct
