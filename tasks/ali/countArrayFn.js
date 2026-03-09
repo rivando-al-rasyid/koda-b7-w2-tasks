938d9c4460623a4b9f92face17424f3850626493
l1mus <alinesezcore@gmail.com>
2026-03-09 13:20:44 +0700

    refactor: add error handling

--- tasks/ali/countArrayFn.js
+++ tasks/ali/countArrayFn.js
@@ -2,7 +2,7 @@ export const countArray = (data) => {
     let result = 1 ;
     for(let i = 0 ; i < data.length ; i++){
         if(typeof data[i] !== "number"){
-            return `data input tidak valid pada index ke-${i} = ${data[i]}`;
+            throw new Error(`data input tidak valid pada index ke-${i} = ${data[i]}`);
         }else{
             result *= data[i] ;
         }
--- tasks/ali/printTable.js
+++ tasks/ali/printTable.js
@@ -2,7 +2,7 @@ export const cetakTabel = (data) =>{
     console.log(typeof data)
     const result = []
     if(typeof data !== "number" || data < 0){
-        return "Input tidak valid , harus Nomor"
+        throw new Error("Input tidak valid , harus Nomor")
     }
     for(let i = 1 ; i <= 10 ; i++){
         result.push(`${data} x ${i} = ${data*i}`)
