3a5287cca550843a8a0850f6bd5cb035dc994831
rivando-al-rasyid <nougat-tutorial@protonmail.com>
2026-03-09 15:05:03 +0700

    chore : add space nomor4.js

--- tasks/rivando/nomor4.js
+++ tasks/rivando/nomor4.js
@@ -3,9 +3,12 @@
 //    format maupun karena Object itu sendiri belum di buat
 
 // b. karena array tidak memiliki isi(kosong) menyebabkan menghitung berapa banyak value dalam array tidak bisa di lakukan
+
 // c. adanya sebuah tutup kurung tidak di gunakan dalam code menyebabkan error
+
 // d. mirip dengan jawaban b tetapi dengan Object karena tidak adanya sebuath objek yang bernama status di dalam objek
 //    setting menybabkan tersebut
+
 // e. saya belum tau error ini tapi dari namanya saya ber ansum sikan penyebabnya function berjalan melebihi
 //    kapasitas kode/hardware yang di sediakan dan dari nama fuction bahwasnya ada kemungkinan fuction berjalan berulang
 //    berarti ada kesalahan dalam perulangan
